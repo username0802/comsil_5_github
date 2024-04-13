27aebca63bdb43414faf4d448863194e934bcc88
cse20231834 <min3959@naver.com>
2024-04-13 22:33:16 +0900

    third branch

--- main.cpp
+++ main.cpp
@@ -17,14 +17,13 @@ int main(){
 		cout<<"자료구조 선택(1: Stack, Other: Linked List): ";
 		cin>>mode;
 						    
-		if(mode == 1) {
-			cout<<"Stack 구조로 데이터를 저장합니다."<<endl;
+		if(mode == 1)
 			p = new Stack<int>();    // 정수를 저장하는 스택
-		}
 			
-		else
+		else {
+			cout<<"Linked List 구조로 데이터를 저장합니다."<<endl;
 			p = new LinkedList<int>();
-
+		}
 
 		do{
 			prnMenu();
